c1409bd8a0090f0dfa494ca321ce1e07420325b0
SURFEIT\David <david.coats@gmail.com>
2025-03-23 11:47:23 -0700

    Add marker attribute types.

---
+++ source/F10Y.T0009/Code/Attributes/HasXMarkerAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0009
+{
+    /// <summary>
+    /// Attribute indicating an interface type is an IHasX type.
+    /// IHasX interfaces should only have a single property with a only a getter, but with freedom to choose the property name.
+    /// </summary>
+    /// <remarks>
+    /// The marker attribute is useful for surveying assemblies and code, then building a catalogue of IHasX types.
+    /// </remarks>
+    /// <seealso cref="N001.HasXMarkerAttribute"/>
+    [AttributeUsage(AttributeTargets.Interface, // HasX types are always interfaces (with a companion dummy implementation class).
+        AllowMultiple = false, // No.
+        Inherited = false // Make inheritors all use the marker type.
+        )]
+    [MarkerAttributeMarker]
+    public class HasXMarkerAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows specifying that a type is *not* an IHasX type.
+        /// This is useful for marking types that end up canonical IHasX type code file locations, but are not actually IHasX types.
+        /// </summary>
+        public bool IsIHasX { get; }
+
+
+        public HasXMarkerAttribute(
+            bool isIHasX = true)
+        {
+            this.IsIHasX = isIHasX;
+        }
+    }
+}
---
+++ source/F10Y.T0009/Code/Attributes/WithXMarkerAttribute-N001.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0009.N001
+{
+    /// <summary>
+    /// <inheritdoc cref="T0009.WithXMarkerAttribute" path="/summary"/>
+    /// <para>
+    /// But, in cases where there are multiple attribtues, this attribute allows specifying both the name and type of X property.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// <inheritdoc cref="T0009.WithXMarkerAttribute" path="/remarks"/>
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Interface, // HasX types are always interfaces (with a companion dummy implementation class).
+        AllowMultiple = false, // No.
+        Inherited = false // Make inheritors all use the marker type.
+    )]
+    [MarkerAttributeMarker]
+    public class WithXMarkerAttribute : Attribute
+    {
+        /// <inheritdoc cref="T0009.WithXMarkerAttribute.IsIWithX"/>
+        public bool IsIWithX { get; }
+
+        public Type TypeOfX { get; }
+
+        public string NameOfX { get; }
+
+
+        public WithXMarkerAttribute(
+            Type typeOfX,
+            string nameOfX,
+            bool isIWithX = true)
+        {
+            this.TypeOfX = typeOfX;
+            this.NameOfX = nameOfX;
+            this.IsIWithX = isIWithX;
+        }
+    }
+}
